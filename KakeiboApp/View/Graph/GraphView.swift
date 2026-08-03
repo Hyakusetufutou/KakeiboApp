0df2da92559e1bc04f6f951a46f5c3bc2d930fa3
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2026-08-03 21:31:12 +0900

    不要なデバッグコードを削除 #47

--- KakeiboApp/View/Graph/GraphView.swift
+++ KakeiboApp/View/Graph/GraphView.swift
@@ -29,7 +29,6 @@ struct GraphView: View {
     }
 
     var body: some View {
-        let _ = Self._printChanges()
         NavigationStack {
             ZStack {
                 Color(.systemGroupedBackground)
