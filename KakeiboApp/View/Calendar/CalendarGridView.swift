5b51cfc6888b561d43c5a5a973877afa07d0b726
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2026-09-21 10:26:21 +0900

    カレンダー上でtransactionを削除できなかったため修正 #81

--- KakeiboApp/View/Calendar/CalendarGridView.swift
+++ KakeiboApp/View/Calendar/CalendarGridView.swift
@@ -49,6 +49,8 @@ struct CalendarGridView: View {
             }
         }
         .padding(.vertical, 8)
+        .listRowSeparatorHiddenAndBackgroundClear()
+        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
     }
 
     private var weekdayHeader: some View {
